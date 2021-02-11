f38f68c147b281b5426807720e862ac41154891e
NehaVerma <neha0305verma@gmail.com>
2021-02-11 20:45:39 +0530

    json log

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,11 +1,15 @@
 package controllers;
 
 import static net.logstash.logback.argument.StructuredArguments.keyValue;
+import static net.logstash.logback.argument.StructuredArguments.kv;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import play.mvc.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 //import views.html.*;  
 /**
@@ -24,10 +28,42 @@ public class HomeController extends Controller {
      * @return
      */
     public Result index() {
+        Order order = new Order("123", "NEW", null);
         logger = LoggerFactory.getLogger(this.getClass());
-        logger.info("test hello info 123", keyValue("name", "value"));
+        logger.info("test hello info 123", keyValue("name", new HashMap<String, Object>() {{
+            put("testmap", "testvalue");
+            put("childMmap", new HashMap<String, Object>() {{
+                put("testmap", "testvalue");
+            }});
+            put("childList", new ArrayList() {{
+                add("123");
+            }});
+        }}), keyValue("order", order));
         System.out.println("print hello test");
         return ok("It works!");
+    }
+    static class Order {
+        String orderId;
+        String status;
+        String canceled;
+
+        Order(String orderId, String status, String canceled) {
+            this.orderId = orderId;
+            this.status = status;
+            this.canceled = canceled;
+        }
+
+        String getOrderId() {
+            return orderId;
+        }
+
+        String getStatus() {
+            return status;
+        }
+
+        String getCanceled() {
+            return canceled;
+        }
     }
 
     /**
